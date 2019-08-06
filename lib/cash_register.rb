cdf8701ac410bb3df060b318a7cd3b3fa77c82a4
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-06 01:35:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     @@cash_register[title] = price
   end
 
-  def apply_discount
+  def apply_discount > 0
     if @discount
       @special_price = (@price * @discount)/100
       @total = @total - @special_price
