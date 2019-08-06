924073fc48c2f52e20d16d845e0e7454cf5a3abc
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-06 01:56:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -35,7 +35,7 @@ class CashRegister
   def void_last_transaction
     @total = @total - @price
     @items.pop
-    if @items.length == nil
+    if @items.length == 0
       @total.to_f = 0.0
     end
   end
